e9f09fe745dfee184ab879b673c8f2b9e84bc82c
diegoepalfaro <diegoepalfaro@gmail.com>
2025-05-09 11:38:25 -0600

    Agregar archivos de proyecto.

---
+++ Jhonattan Sanchez/CRUD.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Jhonattan_Sanchez
+{
+    partial class CRUD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            Textbox = new TextBox();
+            dtg1 = new DataGridView();
+            btnagregar = new Button();
+            btneditar = new Button();
+            btneliminar = new Button();
+            btnsalir = new Button();
+            ((System.ComponentModel.ISupportInitialize)dtg1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(89, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 0;
+            label1.Text = "BUSCAR";
+            // 
+            // Textbox
+            // 
+            Textbox.Location = new Point(164, 38);
+            Textbox.Name = "Textbox";
+            Textbox.Size = new Size(100, 23);
+            Textbox.TabIndex = 1;
+            // 
+            // dtg1
+            // 
+            dtg1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtg1.Location = new Point(89, 112);
+            dtg1.Name = "dtg1";
+            dtg1.Size = new Size(448, 178);
+            dtg1.TabIndex = 2;
+            // 
+            // btnagregar
+            // 
+            btnagregar.Location = new Point(616, 55);
+            btnagregar.Name = "btnagregar";
+            btnagregar.Size = new Size(75, 23);
+            btnagregar.TabIndex = 3;
+            btnagregar.Text = "AGREGAR";
+            btnagregar.UseVisualStyleBackColor = true;
+            // 
+            // btneditar
+            // 
+            btneditar.Location = new Point(619, 99);
+            btneditar.Name = "btneditar";
+            btneditar.Size = new Size(75, 23);
+            btneditar.TabIndex = 4;
+            btneditar.Text = "EDITAR";
+            btneditar.UseVisualStyleBackColor = true;
+            // 
+            // btneliminar
+            // 
+            btneliminar.Location = new Point(629, 152);
+            btneliminar.Name = "btneliminar";
+            btneliminar.Size = new Size(75, 23);
+            btneliminar.TabIndex = 5;
+            btneliminar.Text = "ELIMINAR";
+            btneliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnsalir
+            // 
+            btnsalir.Location = new Point(629, 212);
+            btnsalir.Name = "btnsalir";
+            btnsalir.Size = new Size(75, 23);
+            btnsalir.TabIndex = 6;
+            btnsalir.Text = "button1";
+            btnsalir.UseVisualStyleBackColor = true;
+            // 
+            // CRUD
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnsalir);
+            Controls.Add(btneliminar);
+            Controls.Add(btneditar);
+            Controls.Add(btnagregar);
+            Controls.Add(dtg1);
+            Controls.Add(Textbox);
+            Controls.Add(label1);
+            Name = "CRUD";
+            Text = "CRUD";
+            ((System.ComponentModel.ISupportInitialize)dtg1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox Textbox;
+        private DataGridView dtg1;
+        private Button btnagregar;
+        private Button btneditar;
+        private Button btneliminar;
+        private Button btnsalir;
+    }
+}
---
+++ Jhonattan Sanchez/Formulario.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Jhonattan_Sanchez
+{
+    partial class Formulario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Label label2;
+            label1 = new Label();
+            txtcodigo = new TextBox();
+            txtnombres = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            txtapellidos = new TextBox();
+            txttel = new TextBox();
+            txtdir = new TextBox();
+            btnguardar = new Button();
+            btncancelar = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(168, 123);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Codigo";
+            // 
+            // txtcodigo
+            // 
+            txtcodigo.Location = new Point(170, 142);
+            txtcodigo.Name = "txtcodigo";
+            txtcodigo.Size = new Size(100, 23);
+            txtcodigo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(172, 183);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Nombres";
+            // 
+            // txtnombres
+            // 
+            txtnombres.Location = new Point(172, 208);
+            txtnombres.Name = "txtnombres";
+            txtnombres.Size = new Size(100, 23);
+            txtnombres.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(173, 247);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Apellidos";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(173, 308);
+            label4.Name = "label4";
+            label4.Size = new Size(53, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Telefono";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(174, 366);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 15);
+            label5.TabIndex = 6;
+            label5.Text = "Direccion";
+            // 
+            // txtapellidos
+            // 
+            txtapellidos.Location = new Point(177, 275);
+            txtapellidos.Name = "txtapellidos";
+            txtapellidos.Size = new Size(100, 23);
+            txtapellidos.TabIndex = 7;
+            // 
+            // txttel
+            // 
+            txttel.Location = new Point(181, 336);
+            txttel.Name = "txttel";
+            txttel.Size = new Size(100, 23);
+            txttel.TabIndex = 8;
+            // 
+            // txtdir
+            // 
+            txtdir.Location = new Point(179, 399);
+            txtdir.Name = "txtdir";
+            txtdir.Size = new Size(100, 23);
+            txtdir.TabIndex = 9;
+            // 
+            // btnguardar
+            // 
+            btnguardar.Location = new Point(181, 452);
+            btnguardar.Name = "btnguardar";
+            btnguardar.Size = new Size(75, 23);
+            btnguardar.TabIndex = 10;
+            btnguardar.Text = "Guardar";
+            btnguardar.UseVisualStyleBackColor = true;
+            // 
+            // btncancelar
+            // 
+            btncancelar.Location = new Point(181, 493);
+            btncancelar.Name = "btncancelar";
+            btncancelar.Size = new Size(75, 23);
+            btncancelar.TabIndex = 11;
+            btncancelar.Text = "Cancelar";
+            btncancelar.UseVisualStyleBackColor = true;
+            // 
+            // Formulario
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(474, 580);
+            Controls.Add(btncancelar);
+            Controls.Add(btnguardar);
+            Controls.Add(txtdir);
+            Controls.Add(txttel);
+            Controls.Add(txtapellidos);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(txtnombres);
+            Controls.Add(label2);
+            Controls.Add(txtcodigo);
+            Controls.Add(label1);
+            Name = "Formulario";
+            Text = "Formulario";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtcodigo;
+        private Label label2;
+        private TextBox txtnombres;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txtapellidos;
+        private TextBox txttel;
+        private TextBox txtdir;
+        private Button btnguardar;
+        private Button btncancelar;
+    }
+}
